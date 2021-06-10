5a1916c66282c7094540e7e86b26497f8b3f41fb
John Idogun <sirneij@gmail.com>
2021-06-10 10:54:10 +0100

    Basic traits added

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
 mod to_do;
+use to_do::structs::traits::create::Create;
 use to_do::to_do_factory;
 use to_do::ItemTypes;
 
 fn main() {
     let to_do_item: Result<ItemTypes, &'static str> =
-        to_do_factory("done", "Learn rust programming");
+        to_do_factory("pending", "Learn rust programming");
 
     match to_do_item.unwrap() {
-        ItemTypes::Pending(item) => println!(
-            "This is a pending item with the title: {}",
-            item.super_struct.title
-        ),
+        ItemTypes::Pending(item) => item.create(&item.super_struct.title),
         ItemTypes::Done(item) => println!(
-            "This is done item with the title: {}",
+            "It's a done item with the title: {}",
             item.super_struct.title
         ),
     }
